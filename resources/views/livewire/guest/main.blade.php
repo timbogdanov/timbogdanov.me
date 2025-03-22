96815e0791250e39c4db273d54e380a31a7622c8
tim-bogdanov <timb@snapsupplements.com>
2025-03-22 14:11:33 -0500

    update

--- resources/views/livewire/guest/main.blade.php
+++ resources/views/livewire/guest/main.blade.php
@@ -4,12 +4,17 @@ use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
 new #[Layout('components.layouts.guest')] class extends Component {
+    public $lastLogin;
 
+    public function mount()
+    {
+        $this->lastLogin = now()->format('D M d H:i:s');
+    }
 }; ?>
 
 <div class="container mx-auto">
     <div class="py-2 dark:bg-[#141414] dark:text-[#6E6E6E] font-mono text-xs">
-        Last login: Sat Mar 15 14:30:05
+        Last login: {{ $lastLogin }}
     </div>
 </div>
 
