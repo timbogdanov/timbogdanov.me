d09b21646a0e46e72d846951c5c726757e3a37bf
tim-bogdanov <timb@snapsupplements.com>
2025-03-22 23:02:40 -0500

    update

--- resources/views/livewire/guest/main.blade.php
+++ resources/views/livewire/guest/main.blade.php
@@ -5,17 +5,19 @@ use Livewire\Volt\Component;
 
 new #[Layout('components.layouts.guest')] class extends Component {
     public $lastLogin;
+    public $ttys;
 
     public function mount()
     {
         $this->lastLogin = now()->format('D M d H:i:s');
+        $this->ttys = rand(0,9);
     }
 }; ?>
 
 <div class="dark:bg-[#141414]">
     <div class="container mx-auto">
         <p class="py-2 dark:text-[#6E6E6E] font-mono text-xs">
-            Last login: {{ $lastLogin }} on ttys005
+            Last login: {{ $lastLogin }} on ttys00{{ $ttys }}
         </div>
     </div>
 </div>
