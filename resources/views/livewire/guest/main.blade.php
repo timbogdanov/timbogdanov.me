e02f6f76b4a0a134caf26bf94839d7d58a587fb3
tim-bogdanov <timb@snapsupplements.com>
2025-03-23 11:48:55 -0500

    update

--- resources/views/livewire/guest/main.blade.php
+++ resources/views/livewire/guest/main.blade.php
@@ -10,16 +10,31 @@ new #[Layout('components.layouts.guest')] class extends Component {
     public function mount()
     {
         $this->lastLogin = now()->format('D M d H:i:s');
-        $this->ttys = rand(0,9);
+        $this->ttys = rand(0, 9);
     }
 }; ?>
 
-<div class="dark:bg-[#141414]">
-    <div class="container mx-auto">
-        <p class="py-2 dark:text-[#6E6E6E] font-mono text-xs">
-            Last login: {{ $lastLogin }} on ttys00{{ $ttys }}
+<div>
+    <div class="dark:bg-[#141414]">
+        <div class="container mx-auto">
+            <p class="py-2 dark:text-[#6E6E6E] font-mono text-xs">
+                Last login: {{ $lastLogin }} on ttys00{{ $ttys }}
+        </div>
+    </div>
+
+    <div class="container mx-auto my-3">
+        <div class="flex items-center justify-between">
+            <div class="flex items-center">
+                <img src="/assets/logo.svg" alt="" class="mr-2">
+                <h1 class="font-mono text-sm font-bold">tim bogdanov</h1>
+            </div>
+
+            <div>
+                email
+                telegram
+                github
+                twitter
+            </div>
         </div>
     </div>
 </div>
-
-
