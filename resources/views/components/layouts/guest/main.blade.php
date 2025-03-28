4cbc7477962267aa11a2bb4c286e3651aab035d6
tim-bogdanov <timb@snapsupplements.com>
2025-03-28 13:28:11 -0500

    wip

--- resources/views/components/layouts/guest/main.blade.php
+++ resources/views/components/layouts/guest/main.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 <head>
-    @include('partials.head')
+    @include('partials.head', ['title' => 'tim bogdanov'])
 </head>
 <body class="bg-[#FDFDFC] dark:bg-[#121212] text-[#1b1b18] dark:text-[#E5DDD1]">
 {{ $slot }}
